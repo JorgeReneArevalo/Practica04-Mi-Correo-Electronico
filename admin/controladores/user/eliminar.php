47245e5ca2d8b295cba4e4a5d570cb6eefd72e20
Jorge Arevalo <jorgearevalo.097@gmail.com>
2019-05-22 19:09:08 -0500

    Actualizar

--- admin/controladores/user/eliminar.php
+++ admin/controladores/user/eliminar.php
@@ -7,6 +7,24 @@
     <title>Eliminar usuario</title>
 </head>
 <body>
+    <?php
+        include '../../../config/conexionBD.php'; 
+        $codigo = $_POST["codigo"];  
+        date_default_timezone_set("America/Guayaquil"); 
+        $fecha = date('Y-m-d H:i:s', time()); 
+        $sql = "UPDATE usuario SET usu_eliminado = 'S', usu_fecha_modificacion = '$fecha' WHERE usu_codigo = $codigo"; 
+
+        if ($conn->query($sql) === TRUE) { 
+        } else {
+            echo "<p>Error: " . $sql . "<br>" . mysqli_error($conn) . "</p>"; 
+        } else {
+        echo "<p>Error: " . $sql . "<br>" . mysqli_error($conn) . "</p>"; 
+        } 
+        $conn->close(); 
+    ?>
+    <div class="button">
+        <br>
+    </div>
     
 </body>
 </html>
--- admin/controladores/user/enviar.php
+++ admin/controladores/user/enviar.php
@@ -1,9 +1,8 @@
-
-<?php 
-session_start(); 
-    if(!isset($_SESSION['isLogged']) || $_SESSION['isLogged'] === FALSE){ 
-        header("Location: /GestionDeUsuarios/public/vista/login.html"); 
-        } 
+<?php
+session_start();
+if (!isset($_SESSION['isUser']) || $_SESSION['isUser'] === FALSE) {
+    header("Location: /Practica04/public/vista/login.html");
+}
 ?>
 
 
@@ -22,150 +21,89 @@ session_start();
                 
 </head>
 <body>
-                  <?php 
-                    //CONEXION A LA BASE DE DATOS
-                    include '../../../config/conexionBD.php';
-                  //RECUPERO EL CORREO DEL USUARIO INGRESADO
-                    $usuario=$_SESSION['user'];
-
-                    $sql="SELECT * FROM usuario WHERE usu_correo = '$usuario' ";
-                     //Enviar una consulta MySQL
-                     $result=$conn->query($sql); 
-                      //Recupera una fila de resultados como un array asociativo
-                    $resultarr=mysqli_fetch_assoc($result);
-                    //Obtnemos el valo de una fila especifico
-                    ?>
-
-                    
-        <div id ="contenido">  
-                <nav > 
-                    <ul class="nav" >
-                        <li><a href="../../vista/usuario/paginaUsuario.php">INICIO</a></li>
-                       
-                                <?php 
-                                 $usuario = $resultarr["usu_correo"];
-                                 $cad1 = "../../vista/usuario/enviarCorreo.php?usuario=";
-                                 $final1 = $cad1 . $usuario;
-
-                                ?>
-                        <li><a href="<?php echo $final1 ?>">NUEVO MENSAJE</a></li>
-
-                                <?php 
-                                 $codigo = $resultarr["usu_codigo"];
-                                 $cad1 = "../../vista/usuario/listaMensajesEnviados.php?usuario=";
-                                 $final = $cad1 . $usuario;
-
-                                 ?>
-
-
-                        <li><a href= "<?php echo $final ?>" >MENS. ENVIADOS </a></li>
-
-                        <li><a >MI CUENTA</a>
-                            <ul>     
-                                 <?php 
-                                 $codigo = $resultarr["usu_codigo"];
-                                 $cad1 = "../../vista/usuario/modificar.php?codigo=";
-                                 $cad2 = $codigo;
-                                 $final1 = $cad1 . $cad2;
-
-                                 $cad3 = "../../vista/usuario/cambiar_contrasena.php?codigo=";
-                                 $final2= $cad3 . $cad2;
-
-                                 $cad4 = "../../vista/usuario/eliminar.php?codigo=";
-                                 $final3= $cad4 . $cad2;
-
-
-                                 ?>
-                                    <li><a href= "<?php echo $final1 ?>" >DATOS </a></li>
-
-                                    
-                                    <li><a href= "<?php echo $final3 ?>" >ELIMINAR </a></li>
-
-
-                                    <li><a href="<?php echo $final2 ?>"> CONTRASEÑA </a></li>
-                            </ul>
-                         </li>
-                        
-                         <li><a href='../../../public/vista/login.html' >CERRAR SESIÓN </a>
-                        
-                     </ul>
-                </nav>
-        </div>  
-        
+  <?php 
+    include '../../../config/conexionBD.php';
+    $usuario=$_SESSION['user'];
+    $sql="SELECT * FROM usuario WHERE usu_correo = '$usuario' ";
+    $result=$conn->query($sql); 
+    $resultarr=mysqli_fetch_assoc($result);
+  ?>
   
-    
-         <article>
-                   <h1>ESTADO DE SU MENSAJE :)</h1>
-                   <?php  
+  <div id ="contenido">  
+    <nav > 
+      <ul class="nav" >
+        <li><a href="../../vista/user/index.php">INICIO</a></li>
+          <?php 
+            $usuario = $resultarr["usu_correo"];
+            $cad1 = "../../vista/user/enviarCorreo.php?usuario=";
+            $final1 = $cad1 . $usuario;
 
-// Llamando a los campos
-date_default_timezone_set("America/Guayaquil"); 
-$fecha = date('Y-m-d H:i:s', time()); 
-$remitente = $_POST['remitente'];
-$destinatario = $_POST['destinatario'];
-$asunto = $_POST['asunto'];
-$mensaje = $_POST['mensaje'];
+            ?>
+          <li><a href="<?php echo $final1 ?>">NUEVO MENSAJE</a></li>
+            <?php 
+              $codigo = $resultarr["usu_codigo"];
+              $cad1 = "../../vista/user/mensajeEnviado.php?usuario=";
+              $final = $cad1 . $usuario;
+            ?>
+          <li><a href= "<?php echo $final ?>" >MENS. ENVIADOS </a></li>
+            <li><a >MI CUENTA</a>
+              <ul>     
+                <?php
+                  $codigo = $resultarr["usu_codigo"];
+                  $cad1 = "../../vista/user/modificar.php?codigo=";
+                  $cad2 = $codigo;
+                  $final1 = $cad1 . $cad2;
 
-include '../../../config/conexionBD.php';
+                  $cad3 = "../../vista/user/cambiar_contraseña.php?codigo=";
+                  $final2= $cad3 . $cad2;
 
-$sql="SELECT usu_rol FROM usuario WHERE usu_correo = '$destinatario' ";
-//Enviar una consulta MySQL
-$result=$conn->query($sql); 
-//Recupera una fila de resultados como un array asociativo
-$resultarr=mysqli_fetch_assoc($result);
-//Obtnemos el valo de una fila especifica
-$attempts = $resultarr["usu_rol"];
-
-if($attempts == 'admin'){
-
-       echo "NO SE PUEDE ENVIAR , EL CORREO PERTENECE A UN USUARIO ADMINISTRADOR ";
-      // echo "<a href='../vista/enviarCorreo.php'>REGRESAR </a>";
-      ///header("Location: ../../../public/vista/login.html");
-      
-}else{
-
-// Datos para el correo
-$carta = "Fecha: $fecha \n";
-$carta .= "De: $remitente \n";
-$carta .= "Mensaje: $mensaje";
-
-// Enviando Mensaje
-//mail($destinatario, $asunto, $carta);
-
-
-$sql = "INSERT INTO correos VALUES (0,'$fecha', '$remitente','$destinatario','$asunto', '$mensaje')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "ENVIADO CON ÉXITO ";
-    //echo "<a href='../vista/enviarCorreo.php'>REGRESAR </a>";
-   /// header('Location:../vista/enviarCorreo.php');
-
-    } else {
+                  $cad4 = "../../vista/user/eliminar.php?codigo=";
+                  $final3= $cad4 . $cad2;
+                ?>
+                <li><a href= "<?php echo $final1 ?>" >DATOS </a></li>               
+                <li><a href= "<?php echo $final3 ?>" >ELIMINAR </a></li>
+                <li><a href="<?php echo $final2 ?>"> CONTRASEÑA </a></li>
+              </ul>
+            </li>
+            <li><a href='../../../public/vista/login.html' >CERRAR SESIÓN </a>
+        </ul>
+    </nav>
+  </div>  
+    <article>
+      <h1>ESTADO DE SU MENSAJE :)</h1>
+      <?php  
+        date_default_timezone_set("America/Guayaquil"); 
+        $fecha = date('Y-m-d H:i:s', time()); 
+        $remitente = $_POST['remitente'];
+        $destinatario = $_POST['destinatario'];
+        $asunto = $_POST['asunto'];
+        $mensaje = $_POST['mensaje'];
         
-    echo "<p class='error'>Error: " . mysqli_error($conn) . "</p>";
-    }
+        include '../../../config/conexionBD.php';
 
-    $conn->close(); 
-}
+        $sql="SELECT usu_rol FROM usuario WHERE usu_correo = '$destinatario' ";
+        $result=$conn->query($sql); 
+        $resultarr=mysqli_fetch_assoc($result);
+        $attempts = $resultarr["usu_rol"];
 
- ?>
-    
+        if($attempts == 'admin'){
+          echo "NO SE PUEDE ENVIAR , EL CORREO PERTENECE A UN USUARIO ADMINISTRADOR ";
+        }else{
+          $carta = "Fecha: $fecha \n";
+          $carta .= "De: $remitente \n";
+          $carta .= "Mensaje: $mensaje";
+          $sql = "INSERT INTO correos VALUES (0,'$fecha', '$remitente','$destinatario','$asunto', '$mensaje')";
 
+        if ($conn->query($sql) === TRUE) {
+            echo "ENVIADO CON ÉXITO ";
+            } else { 
+            echo "<p class='error'>Error: " . mysqli_error($conn) . "</p>";
+            }
+            $conn->close(); 
+        }
+      ?>
+    </article>
+  </div>
 
-       
-        </article>
-
-
-
-        </div>
-
-        <footer>
-
-        <p >&copy; TODOS LOS DERECHOS RESERVADOS</p>
-        <p ></Strong> Franklin Gustavo Guallpa Giñin  <Strong> </p>
-        <p ></Strong> 2019 <Strong> </p>
-                    
-        </footer>
-    
 </body>
 </html>
